fa86e51b8d4801170790c653cb6dd95262cfca1a
torrance-k <knapptori6@gmail.com>
2025-11-13 10:59:43 -0500

    fix: environment was right, needed CORS implementation

--- package/exercise1/api/Program.cs
+++ package/exercise1/api/Program.cs
@@ -9,6 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularClient", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -33,6 +43,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngularClient"); 
 
 app.UseAuthorization();
 
--- package/stargate-ui/src/environments/environment.ts
+++ package/stargate-ui/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:5000'
+  apiBaseUrl: 'http://localhost:5204'
 };
 
 /*
